00736e60e3bddefbf2fcb030da0ec96eeb757060
mohammed kaif <mkaif555123@gmail.com>
2024-08-19 15:52:12 +0500

    js logic start

---
+++ script.js
@@ -0,0 +1,15 @@
+const button = document.getElementById("btn");
+const playAgain = document.getElementById("play-again");
+
+let turnO = true;
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+]
